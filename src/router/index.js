708b92cdeb7bedde40574a2cd0a2010c97c619ab
nanangtokenku <81137154+nanangtokenku@users.noreply.github.com>
2023-01-24 10:12:31 +0800

    fetch route from db

--- src/router/index.js
+++ src/router/index.js
@@ -1,77 +1,82 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-//import VueRouter from 'vue-router'
-// const routes = []
+const routes = []
 
-// const requestOptions = {
-//   method: "GET",
-//   withCredentials: true,
-//   headers: {
-//     //"Content-Type": "application/json",
-//     "x-api-key":"A7980EB02ADE9DD9FD90DDAB0AEF1676",
-//     "x-token": 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoiMzE5OCJ9LCJpYXQiOjE2NzQ0MzYyOTQsImV4cCI6MTY3NDUyMjY5NH0.IkuqwfBY3dCJxUEFMZ3L4bg3AHu1TsuWPCcYFVCN8v4'
-//   },
-//   body: null,
-// };
+const requestOptions = {
+  method: "GET",
+  withCredentials: true,
+  headers: {
+    //"Content-Type": "application/json",
+    "x-api-key":"A7980EB02ADE9DD9FD90DDAB0AEF1676",
+    "x-token": 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoiMzE5OCJ9LCJpYXQiOjE2NzQ1MjUzNzksImV4cCI6MTY3NDYxMTc3OX0.NSjrCMix2ByxK709UGm9nQXHOQo7r9X8knjPLzvqKO4'
+  },
+  body: null,
+};
 
-// let response = await fetch(
-//   "https://pps-stie-nobel.org/api/truck_menu/all",
-//   requestOptions
-// )
-// .then(response => response.json())
-//   .then(data => {
+let response = await fetch(
+  "https://pps-stie-nobel.org/api/truck_menu/all",
+  requestOptions
+)
+.then(response => response.json())
+  .then(data => {
 
-//       routes.push({
-//         path: '/',
-//         name: 'Home',
-//        component: () => import('../views/Home.vue'),
-//       })
+    routes.push({
+      path: '/',
+      name: 'Home',
+     component: () => import('../views/Home.vue'),
+    }),
 
-//        data.data.truck_menu.forEach((item, index) => {
+    routes.push({
+      path: '/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue'),  
+    }),
+
+    data.data.truck_menu.forEach((item, index) => {
       
-//           routes.push({
-//             path: item.path,
-//             name: "AAA",
-//             //component: require(`./views/${item.component}.vue`),
-//             component: () => import('../views/AddIncome.vue'),
-//           })
-//       })
-//   })
+          routes.push({
+            path: item.path,
+            name: "AAA",
+            //component: require(`./views/${item.component}.vue`),
+            component: () => import('../views/AddIncome.vue'),
+          })
+      })
+  })
 
 
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),  
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/Register.vue'),
-  },
-  {
-    path: '/add-income',
-    name: 'AddIncome',
-    component: () => import('../views/AddIncome.vue'),
-  },
-  {
-    path: '/add-outcome',
-    name: 'AddOutcome',
-    component: () => import('../views/AddOutcome.vue'),
-  },
-  {
-    path: '/menu/:id',
-    name: 'menu',
-    component: () => import('../views/AddOutcome.vue')
-  }
-]
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: () => import('../views/Home.vue'),
+//   },
+//   {
+//     path: '/login',
+//     name: 'Login',
+//     component: () => import('../views/Login.vue'),  
+//   },
+//   {
+//     path: '/register',
+//     name: 'Register',
+//     component: () => import('../views/Register.vue'),
+//   },
+//   {
+//     path: '/add-income',
+//     name: 'AddIncome',
+//     component: () => import('../views/AddIncome.vue'),
+//   },
+//   {
+//     path: '/add-outcome',
+//     name: 'AddOutcome',
+//     component: () => import('../views/AddOutcome.vue'),
+//   },
+//   {
+//     path: '/menu/:id',
+//     name: 'menu',
+//     component: () => import('../views/AddOutcome.vue')
+//   }
+// ]
 
 const router = createRouter({
   history: createWebHashHistory(),
